f31825c5d763549a3133920bddd9aed32873bcfb
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-03 16:59:26 +0000

    LocationDetails component created and rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "../styles/App.css";
-
-function App() {
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+/* LocationDetails takes 2 props so App wich is the parent needs to give two props that gets contained in the location prop that App takes from index.js where all the data are contained */
+function App({ location }) {
+  const { city, country } = location;
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
 }
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
