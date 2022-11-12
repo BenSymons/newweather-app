5ba3186aed371ce6e0471446b38a0214bfb5f82e
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-12 17:11:10 +0000

    new Forecast Details component created and useState imported in App.js,Forecast Details component rendered in App with the first date available in the forecasts array

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,23 @@ import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
-/* App takes 2 props,LocationDetails will have (location) and ForecastSummaries takes another prop (predictions) */
+import ForecastDetails from "./ForecastDetails";
+import { useState } from "react";
+
+/* App takes 2 props from index.js location and predictions,LocationDetails is taking 2 props city and country and ForecastSummaries takes another prop forecasts */
 function App({ location, predictions }) {
+  const [selectedDate, setSelectedDate] = useState(predictions[0].date);
+  
+
   /* {city,country}= location
-  which is the prop name that App is passing down which is also the data forecast.forecasts
+  location is the prop name that App is taking from index.js and it does contain city and country,
+  predictions which is the prop that App is taking from index.js is = forecast.forecasts which is the array containing the forecats.
   */
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={predictions} />
+      <ForecastDetails forecast={predictions[0]} />
     </div>
   );
 }
@@ -24,6 +32,11 @@ App.propTypes = {
       temperature: PropTypes.shape({
         max: PropTypes.number,
         min: PropTypes.number,
+      }),
+      humidity: PropTypes.number,
+      wind: PropTypes.shape({
+        direction: PropTypes.string,
+        speed: PropTypes.number,
       }),
     })
   ).isRequired,
