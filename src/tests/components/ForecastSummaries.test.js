34e27cef2dbee0939163e559fd82d2193da71658
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-13 17:13:15 +0000

    onSelect and onForecastSelect added as a prop to be rendered in their correspondent controllers

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -12,6 +12,7 @@ describe("ForecastSummaries", () => {
         max: 22,
         min: 12,
       },
+      onSelect: () => {},
     },
     {
       date: 2222222,
@@ -21,16 +22,25 @@ describe("ForecastSummaries", () => {
         max: 24,
         min: 13,
       },
+      onSelect: () => {},
     },
   ];
 
   it("renders correctly", () => {
-    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    const { asFragment } = render(
+      <ForecastSummaries
+        forecasts={validProps}
+        onForecastSelect={validProps.onSelect}
+      />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it("renders the correct number of ForecastSummary instances", () => {
     const { getAllByTestId } = render(
-      <ForecastSummaries forecasts={validProps} />
+      <ForecastSummaries
+        forecasts={validProps}
+        onForecastSelect={validProps.onSelect}
+      />
     );
 
     expect(getAllByTestId("forecast-summary")).toHaveLength(2);
