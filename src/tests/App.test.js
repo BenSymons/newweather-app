9208f590b7745db2d2a3318124db9a5dd1972f65
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-13 20:58:00 +0000

    app snapshot test created and passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
-import getForecast from "../request/getForecast";
 
 describe("App", () => {
-  test("renders App components correctly", () => {
-    render(<App />);
-    const h1Element = screen.getByText({});
-    expect(h1Element).toBeInTheDocument();
+  it("renders correctly", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
