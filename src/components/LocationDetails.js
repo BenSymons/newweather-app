90a63e040654d1aa6935934f3b92b98df107fb83
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-14 16:13:51 +0000

    error handler included in App and in getForecast.js and in handleCitySearch request

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -2,11 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function LocationDetails(props) {
-  const { city, country } = props;
-  return <h1>{`${city}, ${country}`}</h1>;
-}
+  const { city, country, errorMessage } = props;
+  return errorMessage ? (
+    <h1>{errorMessage}</h1>
+  ) : (
+    <h1 className="location-details">{`${city}, ${country}`}</h1>
+  );
+} /* I think the ? after error Message (efter the return) means return the error message if the error message exists */
+LocationDetails.defaultProps = {
+  errorMessage: "",
+};
+
 LocationDetails.propTypes = {
   city: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
 };
 export default LocationDetails;
